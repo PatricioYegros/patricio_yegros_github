b97841cacf46775207bd59aaa6de12691c18094a
Patricio Yegros <patricioyegros@hotmail.com>
2016-06-02 14:47:44 -0300

    mensaje2

---
+++ frontendgloballogic/public/Ejercicio 18/public/index.js
@@ -0,0 +1,41 @@
+var presenter = new presentador();
+
+
+var calculadora=(function(presentador){
+	var valor=1;
+		numero=$container.find('input(type=text)');
+
+	function sumar(input){
+
+
+	}
+	function mostrar(){
+		presentador.resaltarTexto(valor);
+	}
+	function restar(input){
+
+
+	}
+	function dividir(input){
+
+
+	}
+	function multiplicar(input){
+
+
+	}
+	function clear(input){
+
+
+	}
+	function init(){
+
+
+	}
+	return {
+		sumar: sumar,
+		mostrar: mostrar
+	}
+})(presenter);
+
+calculadora.mostrar();
