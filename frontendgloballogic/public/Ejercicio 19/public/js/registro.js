e40f37d563a2ea749af68783b75041ef0ede8bfa
Patricio Yegros <patricioyegros@hotmail.com>
2016-06-08 16:01:19 -0300

    mensaje

--- frontendgloballogic/public/Ejercicio 19/public/js/registro.js
+++ frontendgloballogic/public/Ejercicio 19/public/js/registro.js
@@ -1,6 +1,7 @@
 $(function(){
  var form = $('.container');
  $('.enviar').on('click',addUser);
+ $('.mostrarLista').on('click',listar);
 
  function addUser(e){
  	e.preventDefault();
@@ -38,20 +39,77 @@ $(function(){
 	method:'GET',  
 	success: function(data)
 	{
-		var myDiv = '<div id="ess">';
+		var myDiv = '<tr>' +
+                '<td>' + '<strong>' + 'ID ' + '</strong>' + '</td>' +
+                '<td>' + '<strong>' + 'First Name ' + '</strong>' + '</td>'+
+                '<td>' + '<strong>' + 'Last Name ' + '</strong>' + '</td>'+
+                '<td>' + '<strong>' + 'Gender ' + '</strong>' + '</td>'+
+                '<td>' + '<strong>' + 'Birthday ' + '</strong>' + '</td>'+
+                '<td>' + '<strong>' + 'Address ' + '</strong>' + '</td>'+
+                '<td>' + '<strong>' + 'Photo ' + '</strong>' + '</td>'+
+                '<td>' + '<strong>' + 'Email ' + '</strong>' + '</td>'+
+                '<td>' + '<strong>' + 'Password ' + '</strong>' + '</td>'+
+                '<td>' + '<strong>' + 'Education ' + '</strong>' + '</td>'+
+                '<td>' + '<strong>' + 'Experience ' + '</strong>' + '</td>' + '</tr>';
    	 	for(var i =0 ; i< data.length;i++)
    			{
-    			 var replacing = data[i]._id + data[i].firstName;
-    			 myDiv += replacing;
-  				 myDiv += '<br>'+'<br>';
-   	    	}
-   		myDiv += '</div>'
- 	    $("#jejeje").append(myDiv)
+    			 var replacing = 
+          '<tr>' + '<td>' + data[i]._id + '</td>' +
+          '<td>' + data[i].firstName + '</td>' +
+          '<td>' + data[i].lastName + '</td>' +
+          '<td>' + data[i].gender + '</td>' +
+          '<td>' + data[i].birthday + '</td>' +
+          '<td>' + data[i].address + '</td>' +
+          '<td>' + data[i].photo + '</td>' + 
+          '<td>' + data[i].email + '</td>' +
+          '<td>' + data[i].password + '</td>' +
+          '<td>' + data[i].education + '</td>' + 
+          '<td>' + data[i].experience + '</td>' + 
+          '<td>' + '<button type="button" class="btn btn-danger">' + 'Editar' + '</button>' + '</td>' + '</tr>' ;
+          myDiv += replacing;
+   	    }
+ 	    $(".lista").append(myDiv)
    }
+  });
+  };
 
-
-
-
+  function modificar(e)
+  {
+    e.preventDefault();
+  var user = {
+      _id:'',
+      firstName:'',
+      lastName:'',
+      gender:'',
+      birthday:'',
+      address:'',
+      photo:'',
+      email:'',
+      password:'',
+      education:'',
+      experience:'',
+  };
+  $.ajax({
+    url:'https://connectedin.herokuapp.com/person',
+    method:'GET',
+    success: function(data)
+  {
+    user._id=data[i]._id;
+    user.firstName=data[i].firstName;
+    user.lastName=data[i].lastName;
+    user.gender=data[i].gender;
+    user.birthday=data[i].birthday;
+    user.address=data[i].address;
+    user.photo=data[i].photo;
+    user.email=data[i].email;
+    user.password=data[i].password;
+    user.education=data[i].education;
+    user.experience=data[i].experience;
+  }
+  });
+  $.ajax({
+    url:'https://connectedin.herokuapp.com/person',
+    method:'PUT',
   });
   }
 
