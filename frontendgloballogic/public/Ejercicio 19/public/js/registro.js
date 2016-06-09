29eb1b286611f4a7c85d49fd7b43cb2404e4eb3d
Patricio Yegros <patricioyegros@hotmail.com>
2016-06-09 14:19:56 -0300

    mensaje

--- frontendgloballogic/public/Ejercicio 19/public/js/registro.js
+++ frontendgloballogic/public/Ejercicio 19/public/js/registro.js
@@ -2,7 +2,7 @@ $(function(){
  var form = $('.container');
  $('.enviar').on('click',addUser);
  $('.mostrarLista').on('click',listar);
-
+ 
  function addUser(e){
  	e.preventDefault();
   var user = {
@@ -65,8 +65,9 @@ $(function(){
           '<td>' + data[i].password + '</td>' +
           '<td>' + data[i].education + '</td>' + 
           '<td>' + data[i].experience + '</td>' + 
-          '<td>' + '<button type="button" class="btn btn-danger">' + 'Editar' + '</button>' + '</td>' + '</tr>' ;
+          '<td>' + '<button type="button" class="btn btn-danger editar">' + 'Editar' + '</button>' + '</td>' + '</tr>' ;
           myDiv += replacing;
+          $("#"+i).attr('data-idN',i).on('click',modificar)
    	    }
  	    $(".lista").append(myDiv)
    }
@@ -74,57 +75,31 @@ $(function(){
   };
 
   function modificar(e)
-  {
+  { console.log(data-idN);
+     var idN= $this.attr('data-idN');
     e.preventDefault();
   var user = {
-      _id:'',
-      firstName:'',
-      lastName:'',
-      gender:'',
-      birthday:'',
-      address:'',
-      photo:'',
-      email:'',
-      password:'',
-      education:'',
-      experience:'',
+      firstName: form.find('#nombre').val(),
+      lastName: form.find('#apellido').val(),
+      gender: form.find('#sexo').val(),
+      birthday: form.find('#date').val(),
+      address: form.find('#domicilio').val(),
+      photo: form.find('#foto').val(),
+      email: form.find('#mail').val(),
+      password: form.find('#contraseña').val(),
+      education: form.find('#educacion').val(),
+      experience: form.find('#experiencia').val(),
   };
   $.ajax({
-    url:'https://connectedin.herokuapp.com/person',
-    method:'GET',
-    success: function(data)
-  {
-    user._id=data[i]._id;
-    user.firstName=data[i].firstName;
-    user.lastName=data[i].lastName;
-    user.gender=data[i].gender;
-    user.birthday=data[i].birthday;
-    user.address=data[i].address;
-    user.photo=data[i].photo;
-    user.email=data[i].email;
-    user.password=data[i].password;
-    user.education=data[i].education;
-    user.experience=data[i].experience;
-  }
-  });
-  $.ajax({
-    url:'https://connectedin.herokuapp.com/person',
-    method:'PUT',
+    url:'https://connectedin.herokuapp.com/person' + idN,
+    method:'POST',
+    data: JSON.stringify(user),
+    contentType: 'application/json',
+    success: function(data){
+      alert('User Added');
+   }
   });
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 });
